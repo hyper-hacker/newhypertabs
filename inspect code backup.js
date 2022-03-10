d734fdd806b30968294f0569304d13e96dd3179f
AmongUsInRealLife-SUS-SUS <70657691+hyper-hacker@users.noreply.github.com>
2022-03-10 21:07:04 +0000

    made it work for heroku (i hope) attempt #1

--- inspect code backup.js
+++ inspect code backup.js
@@ -1,13 +1,13 @@
 let url = window.location.href
 let str = url;
 
-console.log(str.substr(str.lastIndexOf("?")+1));
+console.log(str.substr(str.lastIndexOf("%2F%3F")+1));
 
-let newvalue = (str.substr(str.lastIndexOf("?")+1));
+let newvalue = (str.substr(str.lastIndexOf("%2F%3F")+1));
 
 console.log(newvalue);
 
-if (newvalue == 'inspect') {
+if (newvalue == '2F%3Finspect') {
 
 (function(){(function() {var x = document.createElement("script");x.src = "https://luphoria.com/fbl/fbl/firebug-lite-debug.js";x.onload = alert("Loaded Developer Console!");document.head.appendChild(x);})()})();
   
@@ -15,4 +15,4 @@ if (newvalue == 'inspect') {
 
 
 
-//bundle.js
+//bundle.js and document.js
